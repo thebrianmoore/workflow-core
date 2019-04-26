2dce3a236127ae8416f73f98a52e8b6d1173bce4
Brian Moore <bdmcoder@gmail.com>
2019-04-26 12:29:21 -0400

    Updated the Handle method to terminate the workflow if the maximum number of retries has been reached.
    The workflow termination implementation mimics that which is in the terminate handler. Rather than calling the Termination Handler, which would introduce a dependency on it for the retry handler, I pasted the termination code in the new else block.

--- src/WorkflowCore/Services/ErrorHandlers/RetryHandler.cs
+++ src/WorkflowCore/Services/ErrorHandlers/RetryHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using WorkflowCore.Interface;
@@ -11,19 +11,36 @@ namespace WorkflowCore.Services.ErrorHandlers
     {
         private readonly IDateTimeProvider _datetimeProvider;
         private readonly WorkflowOptions _options;
+        private readonly ILifeCycleEventPublisher _eventPublisher;
+
         public WorkflowErrorHandling Type => WorkflowErrorHandling.Retry;
 
-        public RetryHandler(IDateTimeProvider datetimeProvider, WorkflowOptions options)
+        public RetryHandler(IDateTimeProvider datetimeProvider, WorkflowOptions options, ILifeCycleEventPublisher eventPublisher)
         {
             _datetimeProvider = datetimeProvider;
             _options = options;
+            _eventPublisher = eventPublisher;
         }
 
         public void Handle(WorkflowInstance workflow, WorkflowDefinition def, ExecutionPointer pointer, WorkflowStep step, Exception exception, Queue<ExecutionPointer> bubbleUpQueue)
         {
             pointer.RetryCount++;
             pointer.SleepUntil = _datetimeProvider.Now.ToUniversalTime().Add(step.RetryInterval ?? def.DefaultErrorRetryInterval ?? _options.ErrorRetryInterval);
-            step.PrimeForRetry(pointer);
+            if (_options.MaxRetries == 0 || pointer.RetryCount <= _options.MaxRetries)
+                step.PrimeForRetry(pointer);
+            else
+            {
+                workflow.Status = WorkflowStatus.Terminated;
+                _eventPublisher.PublishNotification(new WorkflowTerminated()
+                {
+                    EventTimeUtc = _datetimeProvider.Now,
+                    Reference = workflow.Reference,
+                    WorkflowInstanceId = workflow.Id,
+                    WorkflowDefinitionId = workflow.WorkflowDefinitionId,
+                    Version = workflow.Version
+                });
+            }
+
         }
     }
 }
